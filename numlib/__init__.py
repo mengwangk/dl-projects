54171a218afdbe076163702df098938f8ec4d8f4
Koh Meng Wang <mengwangk@gmail.com>
2019-05-21 09:48:31 +0800

    Initial commit

---
+++ numlib/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+
+import statistics
+import statsmodels.api as sm
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+
+class NumLib():
+    """Number manipulation"""
+
+    def __init__(self):
+        """Constructor
+        """
+        pass
+
+
