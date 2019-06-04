70dd047a7d252873118ba8310e28f20d495e5ec1
Koh Meng Wang <mengwangk@gmail.com>
2019-06-04 23:50:00 +0800

    Updated

--- featurelib/ft_category.py
+++ featurelib/ft_category.py
@@ -6,3 +6,19 @@ featurelib
 Categorical features.
 
 """
+
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import re
+
+from pandas import DataFrame
+from dataclasses import dataclass
+
+from .core import FtAbstract
+
+
+@dataclass()
+class CategoryFt(FtAbstract):
+    pass
